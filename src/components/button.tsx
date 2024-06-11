dc4aeef1d47d525ca6fb874776ff8e90540c1b87
Rafael Bezerra <119262053+leafartech@users.noreply.github.com>
2024-06-11 16:25:11 -0300

    Update button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,5 @@
 export function Button() {
     return (
-        <a href="https://service.certcontrol.com.br/venda-rapida/vd0tp7813tk06567cb65e" className="w-full max-w-sm grad-1 text-center sm:text-lg text-white font-semibold rounded-full py-2 px-6">Quero emitir o meu certificado</a>
+        <a href="https://service.certcontrol.com.br/venda-rapida/vd0tp7813tk66b7842898" className="w-full max-w-sm grad-1 text-center sm:text-lg text-white font-semibold rounded-full py-2 px-6">Quero emitir o meu certificado</a>
     )
 }
